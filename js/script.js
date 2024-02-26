25cab76f8b6a6c38cffc2551e5fa70030f806987
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-02-26 15:21:12 +0100

    [add] change of images at button click in js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const btnArrowUp = document.querySelector(".arrow-up");
+const btnArrowDown = document.querySelector(".arrow-down");
+
 const imagesList = [
   "./assets/img/01.webp",
   "./assets/img/02.webp",
@@ -5,6 +8,8 @@ const imagesList = [
   "./assets/img/04.webp",
   "./assets/img/05.webp",
 ];
+
+let counterImg = 0;
 
 //Inserimento dinamico delle immagini
 for (i = 0; i < imagesList.length; i++) {
@@ -16,4 +21,17 @@ for (i = 0; i < imagesList.length; i++) {
 
 //Inserimento di tutti gli elementi creati in una collection e rimozione classe hide al primo elemento
 const imageCollection = document.querySelectorAll(".img");
-imageCollection[0].classList.remove("hide");
+imageCollection[counterImg].classList.remove("hide");
+
+//Inserimento logica cambio immagini al click
+btnArrowDown.addEventListener("click", function () {
+  imageCollection[counterImg].classList.add("hide");
+  counterImg++;
+  imageCollection[counterImg].classList.remove("hide");
+});
+
+btnArrowUp.addEventListener("click", function () {
+  imageCollection[counterImg].classList.add("hide");
+  counterImg--;
+  imageCollection[counterImg].classList.remove("hide");
+});
